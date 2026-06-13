d345b8d6681f44a2f60643eac0ecd90e1e9643e4
violet-eee <era3972@gmail.com>
2026-06-13 23:46:14 +0600

    Use division function in main program

--- src/main.py
+++ src/main.py
@@ -7,3 +7,8 @@ print("Today's Date:", date.today())
 
 print("10 + 5 =", add(10, 5))
 print("10 - 5 =", subtract(10, 5))
+
+from utils import add, subtract, multiply, divide
+
+print("10 / 5 =", divide(10, 5))
+print("10 / 0 =", divide(10, 0))
