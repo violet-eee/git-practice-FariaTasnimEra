1ca0857e62864a773f3002a74fe72d093c44b407
violet-eee <era3972@gmail.com>
2026-06-13 23:22:13 +0600

    Add basic calculator functions

--- src/main.py
+++ src/main.py
@@ -1,4 +1,9 @@
+
 from datetime import date
+from utils import add, subtract
 
 print("Name: Faria Tasnim Era")
 print("Today's Date:", date.today())
+
+print("10 + 5 =", add(10, 5))
+print("10 - 5 =", subtract(10, 5))
